f7c4aafe87fcdb240ba6f04667891fc936a123a3
Jakub Wojcik <jakub.wojcik@ppuc.pl>
2022-12-12 00:25:12 +0100

    feat: working responsive view

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-console.log("Hello world! 👋");
+const toggleBtn = document.querySelector(".banner__menu-toggle-wrapper");
+const menu = document.querySelector(".banner__menu-wrapper");
+const navLinks = document.querySelectorAll(".banner__menu-item");
+
+const windowWidth = window.innerWidth;
+
+const showMenu = () => {
+  menu.classList.remove("hide");
+};
+
+const hideMenu = () => {
+  menu.classList.add("hide");
+};
+
+const showMenuButton = () => {
+  toggleBtn.classList.remove("hide");
+};
+
+const hideMenuButton = () => {
+  toggleBtn.classList.add("hide");
+};
+
+if (windowWidth >= 999) {
+  showMenu();
+  hideMenuButton();
+} else {
+  hideMenu();
+  showMenuButton();
+}
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth <= 999) {
+    hideMenu();
+    showMenuButton();
+  }
+});
+
+toggleBtn.addEventListener("click", () => {
+  showMenu();
+  hideMenuButton();
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    hideMenu();
+    showMenuButton();
+  });
+});
